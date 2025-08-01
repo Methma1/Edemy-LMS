d3277ee0a2c03ca7d5ff448c3c96dc76642fd367
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-08-01 15:37:21 +0530

    do some changes on navbar

--- src/Components/Student/Navbar.jsx
+++ src/Components/Student/Navbar.jsx
@@ -15,9 +15,13 @@ const {user} = useUser()
       <img src={assets.logo} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div className='flex items-center gap-5'>
-          <button >
+          { user && 
+          <>
+            <button >
             Become Educator</button>|
           <Link to='/my-enrollments'> My Enrollments</Link>
+          </>
+          }
         </div>
         { user ? <UserButton/> :
         <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
@@ -25,11 +29,15 @@ const {user} = useUser()
       </div>
       {/*-modile view-*/}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-        <div>
+        <div className='flex items-center gap-1 sm:gap-2 max-sm:text-sm '>
+          { user && <>
           <button>Become Educator</button>|
-          <Link to='/my-enrollments'> My Enrollments</Link>
+          <Link to='/my-enrollments'> My Enrollments</Link></>}
         </div>
-        <button><img src={assets.user_icon} alt="" /></button>
+        {
+          user ? <UserButton/> :<button onClick={()=>openSignIn()}><img src={assets.user_icon} alt="" /></button>
+        }
+        
       </div>
     </div>
   )
