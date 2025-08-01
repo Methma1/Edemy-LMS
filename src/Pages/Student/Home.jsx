e01cb54a718d7912f9b8c4eac3e6ee57f6d7613e
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-08-01 16:30:20 +0530

    implemented companies component

--- src/Pages/Student/Home.jsx
+++ src/Pages/Student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../Components/Student/Hero'
+import Companies from '../../Components/Student/Companies'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
+      <Companies/>
     </div>
   )
 }
